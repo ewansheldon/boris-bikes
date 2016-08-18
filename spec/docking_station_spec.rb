8513f04664ce8d99c2685c9377f0394c00e60a87
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-18 23:00:02 +0100

    references to bike class in dockingstation test replaced with doubles - tests failing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,10 +18,10 @@ describe DockingStation do
 
   describe '#release_bike' do
 
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+    it 'releases working bikes' do
+      subject.dock double(:bike)
+      bike = subject.release_bike
+      expect(bike).to eq bike
     end
 
     it 'raises an error when there are no bikes available' do
@@ -29,7 +29,7 @@ describe DockingStation do
     end
 
     it 'does not return a broken bike' do
-      bike = Bike.new
+      bike = double(:bike)
       bike.report_broken
       subject.dock(bike)
       expect { subject.release_bike }.to raise_error 'No bikes available'
@@ -40,12 +40,12 @@ describe DockingStation do
   describe '#dock' do
 
     it 'raises an error when full' do
-      subject.capacity.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock double(:bike) }
+      expect { subject.dock double(:bike) }.to raise_error 'Docking station full'
     end
 
     it 'docks something' do
-        bike = Bike.new
+        bike = double(:bike)
         expect(subject.dock(bike)).to eq [bike]
     end
 
