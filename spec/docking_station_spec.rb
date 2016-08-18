d965a40a4f3e95bdba3f0e7bcf484ec0ea7f8c7b
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-18 23:16:01 +0100

    attempted to refactor tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,19 @@ require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+  it 'releases working bikes' do
+    subject.dock double(:bike)
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it 'does not release broken bikes' do
+    bike = double(:bike)
+    bike.report_broken
+    subject.dock bike
+    expect {subject.release_bike}.to raise_error 'No bikes available'
+  end
+
   describe 'initialization' do
 
     it 'has a default capacity' do
@@ -16,38 +29,24 @@ describe DockingStation do
 
   end
 
-  describe '#release_bike' do
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it 'releases working bikes' do
-      subject.dock double(:bike)
-      bike = subject.release_bike
-      expect(bike).to eq bike
-    end
-
+  describe 'release_bike' do
     it 'raises an error when there are no bikes available' do
-    expect { subject.release_bike }.to raise_error 'No bikes available'
-    end
-
-    it 'does not return a broken bike' do
-      bike = double(:bike)
-      bike.report_broken
-      subject.dock(bike)
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
-
   end
 
-  describe '#dock' do
+  describe 'dock' do
+
+    it 'docks something' do
+      bike = double(:bike)
+      expect(subject.dock(bike)).to eq [bike]
+    end
 
     it 'raises an error when full' do
       subject.capacity.times { subject.dock double(:bike) }
-      expect { subject.dock double(:bike) }.to raise_error 'Docking station full'
+      expect { subject.dock double(:bike) }.to raise_error 'Docking Station full'
     end
-
-    it 'docks something' do
-        bike = double(:bike)
-        expect(subject.dock(bike)).to eq [bike]
-    end
-
   end
 end
